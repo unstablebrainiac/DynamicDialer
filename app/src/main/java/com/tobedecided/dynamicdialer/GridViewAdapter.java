caeaa43ed376e3f3f340eb5f0f1c38807558c0b6
Sajal Narang <sajalnarang@gmail.com>
2017-01-11 09:53:02 +0530

    Added Dialpad

---
+++ app/src/main/java/com/tobedecided/dynamicdialer/GridViewAdapter.java
@@ -0,0 +1,74 @@
+package com.tobedecided.dynamicdialer;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class GridViewAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private final int[] digits;
+    private final String[] alphanum;
+
+
+    // 1
+    public GridViewAdapter(Context context, int[] digits,String[] alphanum) {
+        this.mContext = context;
+        this.digits = digits;
+        this.alphanum = alphanum;
+    }
+
+    // 2
+
+    @Override
+    public int getCount() {
+        return digits.length;
+    }
+
+    // 3
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // 4
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        final String alphanumf = alphanum[(position+1)%12];
+        if(convertView==null){
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.digit_layout, null);
+        }
+
+        final TextView nameTextView = (TextView)convertView.findViewById(R.id.number);
+        final TextView alphanumText = (TextView)convertView.findViewById(R.id.alpha_num);
+
+
+        if(position<9) {
+            nameTextView.setText("" + (position+1));
+            alphanumText.setText(alphanumf);
+        }
+        else if(position == 9){
+            nameTextView.setText("*");
+        }
+        else if(position == 10){
+            nameTextView.setText("0");
+            alphanumText.setText(" +");
+        }
+        else if(position == 11){
+            nameTextView.setText("#");
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/tobedecided/dynamicdialer/MainActivity.java
+++ app/src/main/java/com/tobedecided/dynamicdialer/MainActivity.java
@@ -1,15 +1,16 @@
 package com.tobedecided.dynamicdialer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent((Context)MainActivity.this, DialpadActivity.class);
+                startActivity(intent);
+
             }
         });
     }
