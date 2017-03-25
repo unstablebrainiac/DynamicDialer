bfac388c88a5a9fa3f7db91a61f56db4703c5a87
Sajal Narang <sajalnarang@gmail.com>
2017-03-25 17:27:23 +0530

    Ported to BigML(2)

--- app/src/main/java/com/tobedecided/dynamicdialer/PredictionsFragment.java
+++ app/src/main/java/com/tobedecided/dynamicdialer/PredictionsFragment.java
@@ -1,15 +1,19 @@
 package com.tobedecided.dynamicdialer;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -21,11 +25,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.content.ContentValues.TAG;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PredictionsFragment extends Fragment implements Callback<GsonModels.Response> {
+public class PredictionsFragment extends Fragment {
 
     private List<Prediction> predictionList;
     private RecyclerView predictionsRecyclerView;
@@ -49,75 +55,95 @@ public class PredictionsFragment extends Fragment implements Callback<GsonModels
         super.onStart();
 
         Calendar calendar = Calendar.getInstance();
-        int day = 0;
-        switch (calendar.DAY_OF_WEEK) {
-            case Calendar.MONDAY:
-                day = 1;
-                break;
-            case Calendar.TUESDAY:
-                day = 2;
-                break;
-            case Calendar.WEDNESDAY:
-                day = 3;
-                break;
-            case Calendar.THURSDAY:
-                day = 4;
-                break;
-            case Calendar.FRIDAY:
-                day = 5;
-                break;
-            case Calendar.SATURDAY:
-                day = 6;
-                break;
-            case Calendar.SUNDAY:
-                day = 7;
-                break;
-        }
+        int day = calendar.get(Calendar.DAY_OF_WEEK);   //Sunday:1, Saturday:7
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        android.util.Log.d(TAG, "onStart: " + day + " " + hour);
 
-//        String body = "{\"Inputs\": {\"input1\": {\"ColumnNames\": [\"Name\",\"Weekday\",\"Time\",\"Type\"],\"Values\": [[\" \",\"" + day + "\",\"" + calendar.HOUR_OF_DAY + "\",\"OUTGOING\"],]}},\"GlobalParameters\": {}}";
-        String body = "{\"Inputs\": {\"input1\": {\"ColumnNames\": [\"Name\",\"Weekday\",\"Time\",\"Type\",\"Number\",\"Name_Number\"],\"Values\": [[\" \"," + day + "," + calendar.HOUR_OF_DAY + ",\"OUTGOING\",\"0\",\" \"]]}},\"GlobalParameters\": {}}";
+//        String body = "{\"Inputs\": {\"input1\": {\"ColumnNames\": [\"Name\",\"Weekday\",\"Time\",\"Type\",\"Number\",\"Name_Number\"],\"Values\": [[\" \"," + day + "," + calendar.get(Calendar.HOUR_OF_DAY) + ",\"OUTGOING\",\"0\",\" \"]]}},\"GlobalParameters\": {}}";
+        String body = "{\"model\": \"model/58d61016014404745d00031a\",\n" + " \"input_data\":{ \"000001\":\"" + day + "\",\"000004\":\"" + hour + "\"}}";
         RequestBody requestBody = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), body);
         RetrofitInterface retrofitInterface = ServiceGenerator.createService(RetrofitInterface.class);
-        retrofitInterface.getPredictions("2.0", "true", requestBody).enqueue(this);
-    }
-
-    @Override
-    public void onResponse(Call<GsonModels.Response> call, Response<GsonModels.Response> response) {
-        if (response.isSuccessful()) {
-            predictionList = new ArrayList<>();
-            GsonModels.Response data = response.body();
-            GsonModels.Results results = data.getResults();
-            GsonModels.Output1 output1 = results.getOutput1();
-            GsonModels.Value value = output1.getValue();
-            List<String> columnNames = value.getColumnNames();
-            List<String> values = value.getValues().get(0);
-            for (int i = 0; i < 6; i++) {
-                columnNames.remove(0);
-                values.remove(0);
-            }
-            columnNames.remove(columnNames.size() - 1);
-            values.remove(values.size() - 1);
-
-            for (int i = 0; i < columnNames.size(); i++) {
-                predictionList.add(new Prediction(columnNames.get(i).substring(32, columnNames.get(i).length() - 11), columnNames.get(i).substring(columnNames.get(i).length() - 11, columnNames.get(i).length() - 1), Double.parseDouble(values.get(i))));
-            }
-            Collections.sort(predictionList, Prediction.Comparators.PROBABILITY);
-            predictionsAdapter = new PredictionsAdapter(predictionList, getActivity(), new ItemClickListener() {
-                @Override
-                public void onItemClick(View v, int position) {
-                    //TODO: Handle Prediction clicks
+//        retrofitInterface.getPredictions("2.0", "true", requestBody).enqueue(this);
+        retrofitInterface.getPredictions("application/json", "bhavesh2109", "b5265ab5defd322e797052263c4f04e1bcb53d42", requestBody).enqueue(new Callback<GsonModels.BigMLResponse>() {
+            @Override
+            public void onResponse(Call<GsonModels.BigMLResponse> call, Response<GsonModels.BigMLResponse> response) {
+                if (response.isSuccessful()) {
+                    predictionList = new ArrayList<>();
+                    android.util.Log.d(TAG, "onResponse: " + response.code());
+                    GsonModels.BigMLResponse bigMLResponse = response.body();
+                    List<List<String>> probabilities = bigMLResponse.getProbabilities();
+                    for (int i = 0; i < probabilities.size(); i++) {
+                        predictionList.add(new Prediction(probabilities.get(i).get(0), probabilities.get(i).get(0), Double.parseDouble(probabilities.get(i).get(1))));
+                    }
+                    Collections.sort(predictionList, Prediction.Comparators.PROBABILITY);
+                    predictionsAdapter = new PredictionsAdapter(predictionList, getActivity(), new ItemClickListener() {
+                        @Override
+                        public void onItemClick(View v, int position) {
+                            Intent intent = new Intent(Intent.ACTION_CALL);
+                            intent.setData(Uri.parse("tel:" + predictionList.get(position).getNumber()));
+                            while (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, LogsAdapter.MY_PERMISSIONS_CALL_PHONE);
+                            }
+                            startActivity(intent);
+                        }
+                    });
+                    GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+                    predictionsRecyclerView = (RecyclerView) getActivity().findViewById(R.id.predictions_list);
+                    predictionsRecyclerView.setLayoutManager(layoutManager);
+                    predictionsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+                    predictionsRecyclerView.setAdapter(predictionsAdapter);
                 }
-            });
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-            predictionsRecyclerView = (RecyclerView) getActivity().findViewById(R.id.predictions_list);
-            predictionsRecyclerView.setLayoutManager(layoutManager);
-            predictionsRecyclerView.setItemAnimator(new DefaultItemAnimator());
-            predictionsRecyclerView.setAdapter(predictionsAdapter);
-        }
+            }
+
+            @Override
+            public void onFailure(Call<GsonModels.BigMLResponse> call, Throwable t) {
+                android.util.Log.d(TAG, "onFailure: " + t.toString());
+            }
+        });
     }
 
-    @Override
-    public void onFailure(Call<GsonModels.Response> call, Throwable t) {
-        Toast.makeText(getContext(), "Predictions could not be loaded. Please check your internet connection", Toast.LENGTH_LONG).show();
-    }
+//    @Override
+//    public void onResponse(Call<GsonModels.Response> call, Response<GsonModels.Response> response) {
+//        if (response.isSuccessful()) {
+//            predictionList = new ArrayList<>();
+//            GsonModels.Response data = response.body();
+//            GsonModels.Results results = data.getResults();
+//            GsonModels.Output1 output1 = results.getOutput1();
+//            GsonModels.Value value = output1.getValue();
+//            List<String> columnNames = value.getColumnNames();
+//            List<String> values = value.getValues().get(0);
+//            for (int i = 0; i < 6; i++) {
+//                columnNames.remove(0);
+//                values.remove(0);
+//            }
+//            columnNames.remove(columnNames.size() - 1);
+//            values.remove(values.size() - 1);
+//
+//            for (int i = 0; i < columnNames.size(); i++) {
+//                predictionList.add(new Prediction(columnNames.get(i).substring(32, columnNames.get(i).length() - 11), columnNames.get(i).substring(columnNames.get(i).length() - 11, columnNames.get(i).length() - 1), Double.parseDouble(values.get(i))));
+//            }
+//            Collections.sort(predictionList, Prediction.Comparators.PROBABILITY);
+//            predictionsAdapter = new PredictionsAdapter(predictionList, getActivity(), new ItemClickListener() {
+//                @Override
+//                public void onItemClick(View v, int position) {
+//                    Intent intent = new Intent(Intent.ACTION_CALL);
+//                    intent.setData(Uri.parse("tel:" + predictionList.get(position).getNumber()));
+//                    while (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, LogsAdapter.MY_PERMISSIONS_CALL_PHONE);
+//                    }
+//                    startActivity(intent);
+//                }
+//            });
+//            GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+//            predictionsRecyclerView = (RecyclerView) getActivity().findViewById(R.id.predictions_list);
+//            predictionsRecyclerView.setLayoutManager(layoutManager);
+//            predictionsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//            predictionsRecyclerView.setAdapter(predictionsAdapter);
+//        }
+//    }
+//
+//    @Override
+//    public void onFailure(Call<GsonModels.Response> call, Throwable t) {
+//        Toast.makeText(getContext(), "Predictions could not be loaded. Please check your internet connection", Toast.LENGTH_LONG).show();
+//    }
 }
