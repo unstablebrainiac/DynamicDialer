022c7248237f25018d2b572c42d763457e8ea9e9
Sajal Narang <sajalnarang@gmail.com>
2017-03-25 17:28:22 +0530

    Added GridLayout of PredictionsFragment

--- app/src/main/java/com/tobedecided/dynamicdialer/PredictionsAdapter.java
+++ app/src/main/java/com/tobedecided/dynamicdialer/PredictionsAdapter.java
@@ -1,17 +1,10 @@
 package com.tobedecided.dynamicdialer;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -49,17 +42,6 @@ public class PredictionsAdapter extends RecyclerView.Adapter<PredictionsAdapter.
         final Prediction selectedPrediction = predictionList.get(position);
         holder.nameTv.setText(selectedPrediction.getName());
         holder.numberTv.setText(selectedPrediction.getNumber());
-        holder.callButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:" + selectedPrediction.getNumber()));
-                while (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CALL_PHONE}, LogsAdapter.MY_PERMISSIONS_CALL_PHONE);
-                }
-                context.startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -70,13 +52,11 @@ public class PredictionsAdapter extends RecyclerView.Adapter<PredictionsAdapter.
     public class PredictionsHolder extends RecyclerView.ViewHolder {
         private TextView nameTv;
         private TextView numberTv;
-        private ImageButton callButton;
 
         public PredictionsHolder(View itemView) {
             super(itemView);
             nameTv = (TextView) itemView.findViewById(R.id.name);
             numberTv = (TextView) itemView.findViewById(R.id.number);
-            callButton = (ImageButton) itemView.findViewById(R.id.call_button);
         }
     }
 }
